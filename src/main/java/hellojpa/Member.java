5ab95ab5c574292f0844bd79c5f97fca532e6d4e
hyena0608 <goatsfish@likelion.org>
2021-08-06 17:20:48 +0900

    sequence

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -9,42 +9,13 @@ import java.util.Date;
 public class Member {
 
     @Id //PK 매핑
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
     @Column(name = "name") //DB는 name
     private String username; //객체는 username
 
-    private Integer age;
-
-    @Enumerated(EnumType.STRING) //Enum타입
-    private RoleType roleType;
-
-    @Temporal(TemporalType.TIMESTAMP) //날짜시간
-    private Date createDate;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lasModifiedDate;
-
-
-    // hibernate가 애노테이션 없어도 타입으로 구별해준다.
-    private LocalDate testLocalDate;
-    private LocalDateTime testLocalDateTime;
-
-    @Lob //varchar를 넘어서는 큰 콘텐츠 넣을 때
-    private String description;
-
-    @Transient //DB랑 관계없이 메모리에서 사용용
-    private int temp;
-
     public Member() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getUsername() {
@@ -53,53 +24,5 @@ public class Member {
 
     public void setUsername(String username) {
         this.username = username;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public RoleType getRoleType() {
-        return roleType;
-    }
-
-    public void setRoleType(RoleType roleType) {
-        this.roleType = roleType;
-    }
-
-    public Date getCreateDate() {
-        return createDate;
-    }
-
-    public void setCreateDate(Date createDate) {
-        this.createDate = createDate;
-    }
-
-    public Date getLasModifiedDate() {
-        return lasModifiedDate;
-    }
-
-    public void setLasModifiedDate(Date lasModifiedDate) {
-        this.lasModifiedDate = lasModifiedDate;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public int getTemp() {
-        return temp;
-    }
-
-    public void setTemp(int temp) {
-        this.temp = temp;
     }
 }
