66346021c8c7e4ad51f9b3da4b3f31a0d7720b8d
hyena0608 <goatsfish@likelion.org>
2021-08-10 14:08:02 +0900

    MappedSuperclass

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,36 +1,38 @@
 package hellojpa;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-public class Member {
+public class Member extends BaseEntity {
 
     @Id @GeneratedValue
-    @Column(name = "MEMBER_ID")
-    private Long id;
+        @Column(name = "MEMBER_ID")
+        private Long id;
 
-    @Column(name = "USERNAME") //DB는 name
-    private String username; //객체는 username
+        @Column(name = "USERNAME") //DB는 name
+        private String username; //객체는 username
 
-    @OneToOne
-    @JoinColumn(name = "LOCKER_ID")
-    private Locker locker;
+        @OneToOne
+        @JoinColumn(name = "LOCKER_ID")
+        private Locker locker;
 
-    @OneToMany(mappedBy = "member")
-    private List<MemberProduct> memberProducts = new ArrayList<>();
+        @OneToMany(mappedBy = "member")
+        private List<MemberProduct> memberProducts = new ArrayList<>();
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+        public Long getId() {
+            return id;
+        }
 
-    public String getUsername() {
-        return username;
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getUsername() {
+            return username;
     }
 
     public void setUsername(String username) {
