deb3cae670dc461aec4e9d1c75f285c31b9bbd33
hyena0608 <goatsfish@likelion.org>
2021-08-13 19:23:58 +0900

    값 타입

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -5,11 +5,13 @@ import org.hibernate.Hibernate;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 
 public class JpaMain {
 
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
 
         EntityManager em = emf.createEntityManager();
 
@@ -19,11 +21,36 @@ public class JpaMain {
         try{
 
             Member member = new Member();
-            member.setUsername("hello");
-            member.setHomeAddress(new Address("city", "street", "10000"));
-            member.setWorkPeriod(new Period());
+            member.setUsername("member1");
+            member.setHomeAddress(new Address("homeCity", "street", "10000"));
+
+            member.getFavoriteFoods().add("치킨");
+            member.getFavoriteFoods().add("족발");
+            member.getFavoriteFoods().add("피자");
+
+            member.getAddressHistory().add(new AddressEntity("old1", "street", "10000"));
+            member.getAddressHistory().add(new AddressEntity("newCity1", "street", "10000"));
 
             em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            System.out.println("================== STRAT ==============");
+            Member findMember = em.find(Member.class, member.getId());
+
+            //homeCity -> new City;
+//            findMember.getHomeAddress().setCity("newCity"); // side effect 가능해서 XX
+            Address a = findMember.getHomeAddress();
+            findMember.setHomeAddress(new Address("newCity", a.getStreet(), a.getZipcode()));
+
+            //치킨 -> 한식
+            findMember.getFavoriteFoods().remove("치킨");
+            findMember.getFavoriteFoods().add("치킨");
+
+//            member.getAddressHistory().add(new Address("old1", "street", "10000"));
+//            member.getAddressHistory().add(new Address("old2", "street", "10000"));
+
 
             tx.commit();
         } catch (Exception e) {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -2,34 +2,53 @@ package hellojpa;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 public class Member {
 
     @Id @GeneratedValue
-        @Column(name = "MEMBER_ID")
-        private Long id;
+    @Column(name = "MEMBER_ID")
+    private Long id;
 
-        @Column(name = "USERNAME") //DB는 name
-        private String username; //객체는 username
+    @Column(name = "USERNAME") //DB는 name
+    private String username; //객체는 username
 
+    @Embedded
+    private Address homeAddress;
 
     //기간 Period
     @Embedded
     private Period workPeriod;
 
-    //주소
-    @Embedded
-    private Address homeAddress;
+    @ElementCollection
+    @CollectionTable(name = "FAVORITE_FOOD", joinColumns =
+        @JoinColumn(name = "MEMBER_ID")
+    )
+    @Column(name = "FOOD_NAME")
+    private Set<String> favoriteFoods = new HashSet<String>();
 
-    //주소
-    @Embedded
-    @AttributeOverrides({
-            @AttributeOverride(name="city", column=@Column(name="WORK_CITY")),
-            @AttributeOverride(name="street", column=@Column(name="WORK_STREET")),
-            @AttributeOverride(name="zipcode", column=@Column(name="WORK_ZIPCODE"))
-    })
-    private Address workAddress;
+//    @ElementCollection
+//    @CollectionTable(name = "ADDRESS", joinColumns =
+//        @JoinColumn(name = "MEMBER_ID")
+//    )
+//    private List<Address> addressHistory =  new ArrayList<Address>();
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "MEMBER_ID")
+    private List<AddressEntity> addressHistory = new ArrayList<>();
+
+//    //주소
+//    @Embedded
+//    @AttributeOverrides({
+//            @AttributeOverride(name="city", column=@Column(name="WORK_CITY")),
+//            @AttributeOverride(name="street", column=@Column(name="WORK_STREET")),
+//            @AttributeOverride(name="zipcode", column=@Column(name="WORK_ZIPCODE"))
+//    })
+//    private Address workAddress;
 
     public Long getId() {
         return id;
@@ -61,5 +80,21 @@ public class Member {
 
     public void setHomeAddress(Address homeAddress) {
         this.homeAddress = homeAddress;
+    }
+
+    public Set<String> getFavoriteFoods() {
+        return favoriteFoods;
+    }
+
+    public void setFavoriteFoods(Set<String> favoriteFoods) {
+        this.favoriteFoods = favoriteFoods;
+    }
+
+    public List<AddressEntity> getAddressHistory() {
+        return addressHistory;
+    }
+
+    public void setAddressHistory(List<AddressEntity> addressHistory) {
+        this.addressHistory = addressHistory;
     }
 }
